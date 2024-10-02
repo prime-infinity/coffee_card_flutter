1d3555cf7214867e4cff73408aac9ae732db62d2
prime-infinity <soe.a.prime@gmail.com>
2024-10-02 06:30:31 -0700

    images

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -5,13 +5,29 @@ class CoffeePrefs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
         Row(
-          children: [Text("strength: "), Text("3"), SizedBox(width: 50)],
+          children: [
+            const Text("Strength: "),
+            const Text("3"),
+            Image.asset('assets/img/coffee_bean.png',
+                width: 25,
+                color: Colors.brown[100],
+                colorBlendMode: BlendMode.multiply),
+            const SizedBox(width: 50)
+          ],
         ),
         Row(
-          children: [Text("sugars: "), Text("3"), SizedBox(width: 50)],
+          children: [
+            const Text("Sugars: "),
+            const Text("3"),
+            Image.asset('assets/img/sugar_cube.png',
+                width: 25,
+                color: Colors.brown[100],
+                colorBlendMode: BlendMode.multiply),
+            const SizedBox(width: 50)
+          ],
         )
       ],
     );
