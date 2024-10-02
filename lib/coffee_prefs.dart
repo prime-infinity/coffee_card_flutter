75179615ce7240bc43f0d176cb1f4e245337e31e
prime-infinity <soe.a.prime@gmail.com>
2024-10-02 09:37:34 -0700

    buttons and click events

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class CoffeePrefs extends StatelessWidget {
   const CoffeePrefs({super.key});
 
+  void increaseStrength() {
+    print("inc strength by 1");
+  }
+
+  void increaseSugars() {
+    print("inc sugar by 1");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -16,7 +24,12 @@ class CoffeePrefs extends StatelessWidget {
                 color: Colors.brown[100],
                 colorBlendMode: BlendMode.multiply),
             const Expanded(child: SizedBox()),
-            const Text('+')
+            FilledButton(
+                style: FilledButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    foregroundColor: Colors.white),
+                onPressed: increaseStrength,
+                child: const Text('+'))
           ],
         ),
         Row(
@@ -28,7 +41,12 @@ class CoffeePrefs extends StatelessWidget {
                 color: Colors.brown[100],
                 colorBlendMode: BlendMode.multiply),
             const Expanded(child: SizedBox()),
-            const Text('+')
+            FilledButton(
+                style: FilledButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    foregroundColor: Colors.white),
+                onPressed: increaseSugars,
+                child: const Text('+'))
           ],
         )
       ],
